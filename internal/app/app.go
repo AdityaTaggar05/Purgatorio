a9d7aa9014514830a45c4bd0f689e25d81782cf7
Aditya Taggar <aditya05.mgg@gmail.com>
2026-06-02 21:52:13 +0530

    [server] implement startup and graceful shutdown

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,14 @@
 package app
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/AdityaTaggar05/Purgatorio/internal/config"
 )
@@ -23,5 +30,26 @@ func New(cfg *config.Config) (*App, error) {
 }
 
 func (a *App) Start() error {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		fmt.Println("Purgatorio Server listening on " + a.Server.Addr)
+
+		if err := a.Server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("HTTP server error: %v", err)
+		}
+	}()
+
+	<-stop
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := a.Server.Shutdown(ctx); err != nil {
+		fmt.Printf("Server shutdown failed: %v\n", err)
+		return err
+	}
+
+	fmt.Println("Purgatorio Server stopped gracefully!")
 	return nil
 }
