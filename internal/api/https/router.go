1b181a092451a30048c153143820bb35c0fe1eae
Aditya Taggar <aditya05.mgg@gmail.com>
2026-06-02 22:23:44 +0530

    [fix] superfluous respone writer error

--- internal/api/https/router.go
+++ internal/api/https/router.go
@@ -10,9 +10,9 @@ import (
 func NewRouter(authHandler *auth.AuthHandler) *chi.Mux {
 	r := chi.NewRouter()
 
-	r.Get("/auth/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Service is up and running!"))
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Service is up and running!"))
 	})
 
 	return r
