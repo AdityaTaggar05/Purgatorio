5f66f7d558b6cc392d2cf2457e6150ead4782909
Aditya Taggar <aditya05.mgg@gmail.com>
2026-06-03 20:34:28 +0530

    add custom error constants

---
+++ internal/domain/service/errors.go
@@ -0,0 +1,11 @@
+package service
+
+import "errors"
+
+var (
+	ErrUserAlreadyExists = errors.New("user already exists")
+	ErrInvalidToken = errors.New("token has expired or has been already used")
+	ErrUserNotFound = errors.New("user not found")
+	ErrIncorrectPassword = errors.New("incorrect password")
+)
+
