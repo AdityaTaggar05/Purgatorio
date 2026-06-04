2cc54791df7cd9714e896012542edd4b210c2a88
Aditya Taggar <aditya05.mgg@gmail.com>
2026-06-05 03:33:47 +0530

    enable cors on the backend

--- backend/internal/api/https/router.go
+++ backend/internal/api/https/router.go
@@ -8,12 +8,35 @@ import (
 	"github.com/AdityaTaggar05/Purgatorio/internal/api/https/middleware"
 	"github.com/AdityaTaggar05/Purgatorio/pkg/response"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 )
 
 func NewRouter(logger *slog.Logger, authHandler *auth.AuthHandler) *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestLogger(logger))
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{
+			"http://localhost:5173",
+		},
+
+		AllowedMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+			"DELETE",
+			"OPTIONS",
+		},
+
+		AllowedHeaders: []string{
+			"Accept",
+			"Authorization",
+			"Content-Type",
+		},
+
+		AllowCredentials: true,
+	}))
 
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		response.Success(w, nil, "Service is up and running!")
