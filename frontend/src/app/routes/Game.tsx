3bfcf4365fe2fa917c61125ffadd0ba10e9745dc
Aditya Taggar <aditya05.mgg@gmail.com>
2026-06-05 20:28:27 +0530

    implement a simple GamePage with logout button

--- frontend/src/app/routes/Game.tsx
+++ frontend/src/app/routes/Game.tsx
@@ -1,3 +1,22 @@
+import { useAuth } from "../../hooks/useAuth";
+import * as api from "../../features/auth/authApi"
+
 export default function GamePage() {
-  return <h1>Game</h1>;
+  const { user, logout } = useAuth()
+
+  return <div className="flex items-center justify-center flex-col h-screen bg-[color-purgatory-bg]">
+    <h1> Logged in as {user?.username} </h1>
+    <span>
+      <button onClick={async () => {
+        const ok = await api.logout()
+
+        if (ok) logout()
+      }}
+        type="submit"
+        className="w-full font-serif bg-purgatory-border hover:bg-amber-950/40 border border-amber-500/30 hover:border-amber-500/60 text-gray-200 px-16 py-3 rounded text-sm tracking-widest font-semibold transition-all duration-300 mt-4 shadow-lg hover:cursor-pointer"
+      >
+        Log Out
+      </button>
+    </span>
+  </div>
 }
