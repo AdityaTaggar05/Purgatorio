6a3e2edc0c9dfb10546de1c283c59c4c3225f2da
Aditya Taggar <aditya05.mgg@gmail.com>
2026-06-11 18:49:04 +0530

    implement /user/me api endpoint

--- backend/internal/api/https/user/me.go
+++ backend/internal/api/https/user/me.go
@@ -1,7 +1,23 @@
 package user
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+
+	"github.com/AdityaTaggar05/Purgatorio/internal/domain/service"
+	"github.com/AdityaTaggar05/Purgatorio/pkg/ctxkeys"
+	"github.com/AdityaTaggar05/Purgatorio/pkg/response"
+)
 
 func (h *UserHandler) HandleMe(w http.ResponseWriter, r *http.Request) {
-
+	if me, err := h.Service.GetUserByID(r.Context(), r.Context().Value(ctxkeys.UserID).(string)); err == nil {
+		response.JSON(w, http.StatusOK, me)
+	} else {
+		switch {
+		case errors.Is(err, service.ErrUserNotFound):
+			response.Error(r.Context(), w, http.StatusNotFound, err)
+		default:
+			response.InternalServerError(r.Context(), w, err)
+		}
+	}
 }
