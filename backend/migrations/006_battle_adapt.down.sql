c8208338e7b6891079facea6aafb3b469926a8ce
Aditya Taggar <aditya05.mgg@gmail.com>
2026-06-14 19:25:48 +0530

    [db] fix destruction, loot and duration default values for battles table

--- backend/migrations/006_battle_adapt.down.sql
+++ backend/migrations/006_battle_adapt.down.sql
@@ -5,6 +5,9 @@ ALTER TABLE battles DROP CONSTRAINT IF EXISTS battles_outcome_check;
 ALTER TABLE battles ALTER COLUMN outcome TYPE battle_outcome USING outcome::battle_outcome;
 
 ALTER TABLE battles ALTER COLUMN destruction TYPE INT USING destruction::INT;
+ALTER TABLE battles ALTER COLUMN destruction DROP DEFAULT;
+ALTER TABLE battles ALTER COLUMN loot DROP DEFAULT;
+ALTER TABLE battles ALTER COLUMN duration DROP DEFAULT;
 ALTER TABLE battles DROP COLUMN IF EXISTS finished_at;
 ALTER TABLE battles ALTER COLUMN base_snapshot_id SET NOT NULL;
 
