7006a04a82c68d532bfddb5cdad84f25c096a457
Aditya Taggar <aditya05.mgg@gmail.com>
2026-06-14 18:55:46 +0530

    [fix] http method issue for fetching match list api endpoint

--- backend/internal/api/https/battle/router.go
+++ backend/internal/api/https/battle/router.go
@@ -5,7 +5,7 @@ import "github.com/go-chi/chi"
 func (h *BattleHandler) Routes() chi.Router {
 	r := chi.NewRouter()
 
-	r.Post("/matchlist", h.HandleMatchList)
+	r.Get("/matchlist", h.HandleMatchList)
 	r.Post("/initiate", h.HandleInitiate)
 	r.Get("/{battle_id}/ws", h.HandleWebSocket)
 	r.Get("/{battle_id}/result", h.HandleResult)
