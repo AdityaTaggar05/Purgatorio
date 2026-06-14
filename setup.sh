15b6dae2be28376e1d968241305a56b2cf4ddb84
Aditya Taggar <aditya05.mgg@gmail.com>
2026-06-14 18:49:50 +0530

    [fix] update setup.sh to fix permission errors

--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@ mkdir -p ./backend/keys &&
 openssl genrsa -out ./backend/keys/jwt_private.pem 2048 &&
 openssl rsa -in ./backend/keys/jwt_private.pem -pubout -out ./backend/keys/jwt_public.pem
 
+chmod 755 ./backend/keys
+chmod 644 ./backend/keys/jwt_public.pem
+chmod 644 ./backend/keys/jwt_private.pem
+
 echo PURG_DB_PASS=purg_db_pass123 >> .env
