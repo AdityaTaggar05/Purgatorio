3b3c89f23e5c2c24f70362c980301dd6ebde298b
Aditya Taggar <aditya05.mgg@gmail.com>
2026-06-17 16:23:51 +0530

    update auth middleware to accept access tokens as url param too

--- backend/internal/api/https/middleware/auth.go
+++ backend/internal/api/https/middleware/auth.go
@@ -15,43 +15,46 @@ import (
 )
 
 func RequestAuthenticator(publicKey *rsa.PublicKey) func(http.Handler) http.Handler {
-    return func(next http.Handler) http.Handler {
-        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-            token := extractToken(r)
-            userID, err := parseAndVerify(token, publicKey)
-            if err != nil {
-                response.Error(r.Context(), w, http.StatusUnauthorized, err)
-                return
-            }
-            ctx := context.WithValue(r.Context(), ctxkeys.UserID, userID)
-            next.ServeHTTP(w, r.WithContext(ctx))
-        })
-    }
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			token := extractToken(r)
+			userID, err := parseAndVerify(token, publicKey)
+			if err != nil {
+				response.Error(r.Context(), w, http.StatusUnauthorized, err)
+				return
+			}
+			ctx := context.WithValue(r.Context(), ctxkeys.UserID, userID)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
 }
 
 func parseAndVerify(tokenString string, publicKey *rsa.PublicKey) (uuid.UUID, error) {
-    token, err := jwt.Parse(tokenString, func(t *jwt.Token) (any, error) {
-        return publicKey, nil
-    })
-    if err != nil || !token.Valid {
-        return uuid.Nil, purgerr.Wrap(fmt.Errorf("invalid token"), err)
-    }
-    claims := token.Claims.(jwt.MapClaims)
-    sub, _ := claims["sub"].(string)
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (any, error) {
+		return publicKey, nil
+	})
+	if err != nil || !token.Valid {
+		return uuid.Nil, purgerr.Wrap(fmt.Errorf("invalid token"), err)
+	}
+	claims := token.Claims.(jwt.MapClaims)
+	sub, _ := claims["sub"].(string)
 
-		return uuid.Parse(sub)
+	return uuid.Parse(sub)
 }
 
 func extractToken(r *http.Request) string {
-    authHeader := r.Header.Get("Authorization")
-    if authHeader == "" {
-        return ""
-    }
+	authHeader := r.Header.Get("Authorization")
+	tokenParam := r.URL.Query().Get("token")
 
-    parts := strings.SplitN(authHeader, " ", 2)
-    if len(parts) != 2 || !strings.EqualFold(parts[0], "Bearer") {
-        return ""
-    }
-
-    return parts[1]
+	if authHeader != "" {
+		parts := strings.SplitN(authHeader, " ", 2)
+		if len(parts) != 2 || !strings.EqualFold(parts[0], "Bearer") {
+			return ""
+		}
+		return parts[1]
+	} else if tokenParam != "" {
+		return tokenParam
+	} else {
+		return ""
+	}
 }
